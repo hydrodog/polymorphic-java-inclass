269847b0ee6947f1b3281c849d482a952eb91890
lucymorcos <lucelleaimee@gmail.com>
2017-04-03 15:40:00 -0400

    account

--- src/Account.java
+++ src/Account.java
@@ -2,26 +2,44 @@
 public class Account{
 	
 	private double balance = 100000;
+	private String name;
 	
-	
-	public Account(){
+	public Account(String n){
+		name = n;
+		
+	}
+	public Account(String n, double bal){
+		name = n;
+		balance = bal;
 		
 	}
 	
 	public void buy(int amt, Stock stock){
 		//super(price);
-		double cost = amt * stock.getprice();
+		double cost = amt * stock.getValue();
 		balance-= cost;
+		System.out.println("$" + cost + " has been withdrawn from your account.");
 		
 	}
 	
-	public void sell(int amt, String stock, double price){
-		double tempBalance;
-		tempBalance = amt * price;
-		balance+= tempBalance;
+	public void sell(int amt, Stock stock){
+		
+		double cost = amt * stock.getValue();
+		balance+= cost;
+		System.out.println("$" + cost + " has been deposited into your account.");
+	}
+	
+	public void getBalance(){
+		
+		System.out.println(balance);
 	}
 	
 	public static void main(String[] args) {
+		
+		Account a1 = new Account("Person1");
+		Account a2 = new Account("Person2",540000);
+		a1.getBalance();
+		a2.sell(10, s1);
 	
 	}
 
